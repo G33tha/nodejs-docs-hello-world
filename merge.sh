209e7738d37dc8aa76a49d0c881a1f00e8a40870
G33tha <geetha.p@optit.co>
2019-01-24 13:14:20 +0530

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -5,8 +5,12 @@ git merge --no-ff $CIRCLE_BRANCH
 git config --global push.default matching
 git push https://$CIRCLE_PROJECT_USERNAME:$PASSWORD@github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME
 sleep 5
-curl http://52.172.131.47:3000/status?stageId=stage3&val=1
+curl "http://52.172.131.47:3000/status?stageId=stage3&val=1"
 sleep 5
-curl http://52.172.131.47:3000/status?stageId=stage4&val=1
+curl "http://52.172.131.47:3000/status?stageId=stage4&val=1"
 sleep 5
-curl http://52.172.131.47:3000/status?stageId=stage5&val=1
+curl "http://52.172.131.47:3000/status?stageId=stage5&val=1"
+sleep 5
+curl "http://52.172.131.47:3000/status?stageId=stage6&val=1"
+sleep 5
+curl "http://52.172.131.47:3000/status?stageId=stage7&val=1"
